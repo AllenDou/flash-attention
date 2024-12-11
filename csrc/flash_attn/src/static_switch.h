d52284ddf5285d7c8610aa8e6916780bd1815ce1
zixiao <shunli.dsl@alibaba-inc.com>
2024-12-11 16:20:36 +0800

    update.

--- csrc/flash_attn/src/static_switch.h
+++ csrc/flash_attn/src/static_switch.h
@@ -17,13 +17,8 @@
 
 #define BOOL_SWITCH(COND, CONST_NAME, ...)      \
   [&] {                                         \
-    if (COND) {                                 \
-      constexpr static bool CONST_NAME = true;  \
-      return __VA_ARGS__();                     \
-    } else {                                    \
       constexpr static bool CONST_NAME = false; \
       return __VA_ARGS__();                     \
-    }                                           \
   }()
 
 #ifdef FLASHATTENTION_DISABLE_DROPOUT
@@ -78,37 +73,14 @@
 
 #define FP16_SWITCH(COND, ...)               \
   [&] {                                      \
-    if (COND) {                              \
       using elem_type = cutlass::half_t;     \
       return __VA_ARGS__();                  \
-    } else {                                 \
-      using elem_type = cutlass::bfloat16_t; \
-      return __VA_ARGS__();                  \
-    }                                        \
   }()
 
 #define HEADDIM_SWITCH(HEADDIM, ...)   \
   [&] {                                    \
-    if (HEADDIM <= 32) {                   \
-      constexpr static int kHeadDim = 32;  \
-      return __VA_ARGS__();                \
-    } else if (HEADDIM <= 64) {            \
+    if (HEADDIM <= 64) {            \
       constexpr static int kHeadDim = 64;  \
-      return __VA_ARGS__();                \
-    } else if (HEADDIM <= 96) {            \
-      constexpr static int kHeadDim = 96;  \
-      return __VA_ARGS__();                \
-    } else if (HEADDIM <= 128) {           \
-      constexpr static int kHeadDim = 128; \
-      return __VA_ARGS__();                \
-    } else if (HEADDIM <= 160) {           \
-      constexpr static int kHeadDim = 160; \
-      return __VA_ARGS__();                \
-    } else if (HEADDIM <= 192) {           \
-      constexpr static int kHeadDim = 192; \
-      return __VA_ARGS__();                \
-    } else if (HEADDIM <= 256) {           \
-      constexpr static int kHeadDim = 256; \
       return __VA_ARGS__();                \
     }                                      \
   }()
