91eed5ee38143d4d424884de488aa4040bda2f46
zixiao <shunli.dsl@alibaba-inc.com>
2024-12-24 15:40:20 +0800

    update.

--- csrc/flash_attn/src/flash_fwd_kernel.h
+++ csrc/flash_attn/src/flash_fwd_kernel.h
@@ -901,8 +901,6 @@ inline __device__ void compute_attn_1rowblock_splitkv(const Params &params, cons
     );
 
 #endif
-    // printf("blockIdx.x=%d blockIdx.y=%d blockIdx.z=%d threadIdx.x=%d threadIdx.y=%d threadIdx.z=%d\n", \
-    // blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z);
     if (threadIdx.x == 0 && blockIdx.y == 0 && blockIdx.z == 0) {
         print("\n\n");
     }
