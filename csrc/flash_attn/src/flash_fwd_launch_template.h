56a263e9fbf69025445b78ef2d85989b2abb3db2
zixiao <shunli.dsl@alibaba-inc.com>
2025-01-13 15:10:27 +0800

    update.

--- csrc/flash_attn/src/flash_fwd_launch_template.h
+++ csrc/flash_attn/src/flash_fwd_launch_template.h
@@ -196,7 +196,9 @@ void run_mha_fwd_splitkv_dispatch(Flash_fwd_params &params, cudaStream_t stream)
     // and for headdim 192 with block size 64 x 128.
     // Also for headdim 160 with block size 64 x 128 after the rotary addition.
     constexpr static int kBlockN = Headdim <= 64 ? 256 : (Headdim <= 128 ? 128 : 64);
-    run_flash_splitkv_fwd<Flash_fwd_kernel_traits<Headdim, kBlockM, kBlockN, 1 /*4*/, false, false, T>, Is_causal>(params, stream);
+    params.num_heads = 2;
+    run_flash_splitkv_fwd<Flash_fwd_kernel_traits<Headdim, kBlockM, kBlockN, 1 /*4*/, false, false, T>, \
+                            Is_causal>(params, stream);
 }
 
 
